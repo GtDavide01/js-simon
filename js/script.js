605d3946f579c6739474694b8cc9b86fb554f47e
GtDavide01 <davidefiorini01@gmail.com>
2022-10-19 15:29:19 +0200

    aggiunto bottone per visualizzare i numeri scomparsi

--- js/script.js
+++ js/script.js
@@ -22,31 +22,35 @@ const userNumberHtml = document.getElementById("number-user");
 console.log(userNumberHtml);
 const result = document.getElementById("result");
 const resultTot = document.getElementById("result-tot");
+//recupero bottone html
+const btnVisible = document.getElementById("visible");
 //Creo un array vuoto che conterrà i 5 numeri casuali
 const arrayRandomNum = [];
 //Creo array dove salvo numeri inseriti dall'utente
 const userNumber = [];
 //Ciclo for che si ripete per 5 volte e inserisce il numero all'interno dell'array
-for ( let i = 0 ; i<5 ; i++){
-    let numberGenerate = parseInt(getRandomNumber(1,100));
+for (let i = 0; i < 5; i++) {
+    let numberGenerate = parseInt(getRandomNumber(1, 100));
     arrayRandomNum.push(numberGenerate);
     //Stampo in pagina i valori all'interno dell'array 
-    randomNum.innerHTML += "  "+arrayRandomNum[i]+" ";
+    randomNum.innerHTML += "  " + arrayRandomNum[i] + " ";
 }
 console.log(arrayRandomNum);
 //Richiamo setTimeout per far partire un timer di 30 secondi
-const timer = setTimeout(function(){
-      //Dopo 30 secondi faccio scomparire i numeri visualizzati  a schermo 
-      randomNum.classList.add("none");
-},10000)
-const timerUserNumber = setTimeout(function(){
+const timer = setTimeout(function () {
+    //Dopo 30 secondi faccio scomparire i numeri visualizzati  a schermo 
+    randomNum.classList.add("none");
+}, 10000)
+const timerUserNumber = setTimeout(function () {
     //richiamo funzione per chiedere 5 numeri all'utente
     requestNumber(userNumber);
     //richiamo funzione per confrontare i due array 
-    confrontArray(arrayRandomNum , userNumber);
-},11000)
+    confrontArray(arrayRandomNum, userNumber);
+}, 11000)
 
-
+btnVisible.addEventListener("click",function(){
+    randomNum.classList.remove("none");
+})
 
 
 
@@ -64,32 +68,32 @@ function getRandomNumber(min, max) {
 
 
 //funzione per richiedere 5 numeri all'utente
-function requestNumber (array){
+function requestNumber(array) {
     //Creo ciclo for per chiedere all'utente 5 numeri tramite prompt e li salvo nell'array
-   for( let i  =  0 ; i<5 ; i++){
+    for (let i = 0; i < 5; i++) {
         //Chiedo all'utente il numero tramite prompt
         const numberChoiceUser = parseInt(prompt("Inserisci un numero che ricordi di vaer visto..."));
         //salvo il numero inserito dall'utente nell'array
         array.push(numberChoiceUser);
         //Stampo nell'html i numeri inseriti dall'utente 
-        userNumberHtml.innerHTML += " "+array[i];
-    
+        userNumberHtml.innerHTML += " " + array[i];
+
+    }
 }
-}
-   
+
 //funzione per confrontare due array e restitusci la quantia di numeri uguali e quali
-function confrontArray (array1 , array2){
-        //Confronto i valori dei due Array 
-        let nNumberEquals = 0;
-        for ( let i=0 ; i < array1.length ; i++){
-            let temp = array1[i];
-            for ( let j=0; j < array2.length ; j++){
+function confrontArray(array1, array2) {
+    //Confronto i valori dei due Array 
+    let nNumberEquals = 0;
+    for (let i = 0; i < array1.length; i++) {
+        let temp = array1[i];
+        for (let j = 0; j < array2.length; j++) {
             let temp2 = array2[j];
-            if ( temp == temp2 ){
-            nNumberEquals++;
-            result.innerHTML += " "+temp ;
+            if (temp == temp2) {
+                nNumberEquals++;
+                result.innerHTML += " " + temp;
             }
         }
-        }
-        resultTot.innerHTML += nNumberEquals;
+    }
+    resultTot.innerHTML += nNumberEquals;
 }
